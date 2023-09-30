05bae7cc0a621f525640ea0799267b33325aa52d
Joe Rogan <76793890+joe-rogan-0001@users.noreply.github.com>
2023-09-30 18:51:21 +0100

    escrow stuff

--- ld-beekeeping/fxmanifest.lua
+++
@@ -1,7 +0,0 @@
-fx_version 'cerulean'
-games { 'gta5' }
-
-data_file 'DLC_ITYP_REQUEST' 'stream/np_beehives.ytyp'
-
-client_scripts {'config.lua','client/cl_*.lua'}
-server_scripts {'config.lua', 'server/sv_*.lua'}
