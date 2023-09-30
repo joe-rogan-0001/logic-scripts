46ef262cba27b72033e985557adde74f7e0680a8
Joe Rogan <76793890+joe-rogan-0001@users.noreply.github.com>
2023-09-30 18:48:57 +0100

    APES

--- [standalone]/rcore_spray/client/fonts.lua
+++
@@ -1,9 +0,0 @@
-
-Citizen.CreateThread(function()
-    Wait(100)
-
-    for _, fontData in pairs(FONTS) do
-        RegisterFontFile(fontData.font)
-        RegisterFontId(fontData.font)
-    end
-end)
--- [standalone]/rcore_spray/server/bridge/standalone.lua
+++
@@ -1,8 +0,0 @@
-if Framework.STANDALONE then
-    ShowNotification = function(source, text)
-        TriggerClientEvent('chat:addMessage', source, {
-            template = '<div style="background: rgb(180, 136, 29); color: rgb(255, 255, 255); padding: 5px;">{0}</div>',
-            args = { text }
-        })
-    end
-end
