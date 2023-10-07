6e9668a8e7f4281dcdb236a79fcd7aa182d3d638
Joe Rogan <76793890+joe-rogan-0001@users.noreply.github.com>
2023-10-08 00:39:45 +0100

    payments

---
+++ [lrp]/jim-payments/fxmanifest.lua
@@ -0,0 +1,11 @@
+name "Jim-Payments"
+author "Jimathy"
+version "v2.8.5"
+description "Payment Script By Jimathy"
+fx_version "cerulean"
+game "gta5"
+lua54 'yes'
+
+shared_scripts { 'config.lua', 'shared/*.lua', 'locales/*.lua' }
+client_scripts { 'client/*.lua', }
+server_scripts { '@oxmysql/lib/MySQL.lua', 'server/*.lua' }
