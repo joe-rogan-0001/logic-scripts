976d255b1ffd6b40055265dfca6477b3795a1041
logic-roleplay <146389316+logic-roleplay@users.noreply.github.com>
2023-10-15 02:07:52 +0100

    doorlocks

--- [lrp]/lrp-doorlocks/config.lua
+++ [lrp]/lrp-doorlocks/config.lua
@@ -3514,3 +3514,375 @@ Config.DoorList['yakuza'] = {
     --doorRate = 1.0,
     showNUI = false
 }
+-- vespucciv2clubbygate 
+Config.DoorList['vespucciv2clubbygate'] = {
+    locked = true,
+    objCoords = vec3(-1166.221191, -1186.105591, 4.655549),
+    maxDistance = 6.0,
+    objHeading = 190.41551208496,
+    garage = true,
+    fixText = false,
+    audioRemote = false,
+    lockpick = false,
+    slides = true,
+    objHash = 1586160380,
+    authorizedGangs = { ['savagemc']=0 },
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- vespucciv2entrancedoor 
+Config.DoorList['vespucciv2entrancedoor'] = {
+    locked = true,
+    objCoords = vec3(-1175.177979, -1192.976318, 6.073421),
+    maxDistance = 2.0,
+    objHeading = 190.00001525879,
+    garage = false,
+    fixText = false,
+    audioRemote = false,
+    lockpick = false,
+    slides = false,
+    objHash = 870873065,
+    authorizedGangs = { ['savagemc']=0 },
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- vv2doubleentrancedoor1 
+Config.DoorList['vv2doubleentrancedoor1'] = {
+    locked = true,
+    lockpick = false,
+    audioRemote = false,
+    maxDistance = 2.5,
+    slides = false,
+    doors = {
+        {objHash = -1355256628, objHeading = 279.99996948242, objCoords = vec3(-1178.757446, -1182.726074, 5.897286)},
+        {objHash = -1355256628, objHeading = 99.999977111816, objCoords = vec3(-1178.306763, -1185.281372, 5.897286)}
+    },
+    authorizedGangs = { ['savagemc']=0 },
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- vv2garageentrancedoor1 
+Config.DoorList['vv2garageentrancedoor1'] = {
+    locked = true,
+    objCoords = vec3(-1173.646118, -1173.114624, 6.526886),
+    maxDistance = 6.0,
+    objHeading = 15.000122070313,
+    garage = true,
+    fixText = false,
+    audioRemote = false,
+    lockpick = false,
+    slides = true,
+    objHash = -272364684,
+    authorizedGangs = { ['savagemc']=0 },
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- vv2garageindoordouble1 
+Config.DoorList['vv2garageindoordouble1'] = {
+    locked = true,
+    lockpick = false,
+    audioRemote = false,
+    maxDistance = 2.5,
+    slides = false,
+    doors = {
+        {objHash = -1355256628, objHeading = 9.9999990463257, objCoords = vec3(-1185.699951, -1175.406128, 5.895852)},
+        {objHash = -1355256628, objHeading = 190.00001525879, objCoords = vec3(-1183.151245, -1174.952759, 5.895852)}
+    },
+    authorizedGangs = { ['savagemc']=0 },
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- vv2garageoffice 
+Config.DoorList['vv2garageoffice'] = {
+    locked = true,
+    objCoords = vec3(-1194.548340, -1165.926880, 9.456146),
+    maxDistance = 2.0,
+    objHeading = 190.0,
+    garage = false,
+    fixText = false,
+    audioRemote = false,
+    lockpick = false,
+    slides = false,
+    objHash = -129553421,
+    authorizedGangs = { ['savagemc']=0 },
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- vv2kitchendouble 
+Config.DoorList['vv2kitchendouble'] = {
+    locked = true,
+    lockpick = false,
+    audioRemote = false,
+    maxDistance = 2.5,
+    slides = false,
+    doors = {
+        {objHash = -1355256628, objHeading = 9.9999990463257, objCoords = vec3(-1190.113159, -1159.287842, 5.748736)},
+        {objHash = -1355256628, objHeading = 190.0, objCoords = vec3(-1187.564575, -1158.822510, 5.748736)}
+    },
+    authorizedGangs = { ['savagemc']=0 },
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- vv2roofdoor 
+Config.DoorList['vv2roofdoor'] = {
+    locked = true,
+    objCoords = vec3(-1187.102173, -1139.443970, 14.564635),
+    maxDistance = 2.0,
+    objHeading = 291.16897583008,
+    garage = false,
+    fixText = false,
+    audioRemote = false,
+    lockpick = false,
+    slides = false,
+    objHash = -766681263,
+    authorizedGangs = { ['savagemc']=0 },
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- vv2changerooms 
+Config.DoorList['vv2changerooms'] = {
+    locked = true,
+    objCoords = vec3(-1190.679443, -1150.938599, 5.704319),
+    maxDistance = 2.0,
+    objHeading = 190.00001525879,
+    garage = false,
+    fixText = false,
+    audioRemote = false,
+    lockpick = false,
+    slides = false,
+    objHash = 2089009131,
+    authorizedGangs = { ['savagemc']=0 },
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- vv2secretslidingdoor 
+Config.DoorList['vv2secretslidingdoor'] = {
+    locked = true,
+    objCoords = vec3(-1196.204224, -1152.582031, 4.653471),
+    maxDistance = 2.0,
+    objHeading = 9.9999990463257,
+    garage = true,
+    fixText = false,
+    audioRemote = false,
+    lockpick = false,
+    slides = true,
+    objHash = -861306625,
+    authorizedGangs = { ['savagemc']=0 },
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- vv2churchdouble 
+Config.DoorList['vv2churchdouble'] = {
+    locked = true,
+    lockpick = false,
+    audioRemote = false,
+    maxDistance = 2.5,
+    slides = false,
+    doors = {
+        {objHash = -1210156958, objHeading = 279.99996948242, objCoords = vec3(-1188.289185, -1185.265503, 12.033862)},
+        {objHash = -1210156958, objHeading = 99.999977111816, objCoords = vec3(-1187.834717, -1187.812988, 12.033862)}
+    },
+    authorizedGangs = { ['savagemc']=0 },
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- vv2prezoffice 
+Config.DoorList['vv2prezoffice'] = {
+    locked = true,
+    lockpick = false,
+    audioRemote = false,
+    maxDistance = 2.5,
+    slides = false,
+    doors = {
+        {objHash = -1355256628, objHeading = 280.0, objCoords = vec3(-1189.387085, -1179.222656, 12.035128)},
+        {objHash = -1355256628, objHeading = 99.999977111816, objCoords = vec3(-1188.926025, -1181.778931, 12.030533)}
+    },
+    authorizedGangs = { ['savagemc']=0 },
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- vv2downstairs1 
+Config.DoorList['vv2downstairs1'] = {
+    locked = true,
+    objCoords = vec3(-1188.861328, -1154.671265, 0.889005),
+    maxDistance = 2.0,
+    objHeading = 280.00003051758,
+    garage = false,
+    fixText = false,
+    audioRemote = false,
+    lockpick = false,
+    slides = false,
+    objHash = -766681263,
+    authorizedGangs = { ['savagemc']=0 },
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- vv2downstairs2 
+Config.DoorList['vv2downstairs2'] = {
+    locked = true,
+    objCoords = vec3(-1187.942993, -1159.963013, 0.888894),
+    maxDistance = 2.0,
+    objHeading = 280.00003051758,
+    garage = false,
+    fixText = false,
+    audioRemote = false,
+    lockpick = false,
+    slides = false,
+    objHash = -766681263,
+    authorizedGangs = { ['savagemc']=0 },
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- vv2downstairs3 
+Config.DoorList['vv2downstairs3'] = {
+    locked = true,
+    objCoords = vec3(-1192.076172, -1161.520630, 0.897323),
+    maxDistance = 1.5,
+    objHeading = 190.00001525879,
+    garage = false,
+    fixText = false,
+    audioRemote = false,
+    lockpick = false,
+    slides = false,
+    objHash = -766681263,
+    authorizedGangs = { ['savagemc']=0 },
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- vv2downescape1 
+Config.DoorList['vv2downescape1'] = {
+    locked = true,
+    objCoords = vec3(-1180.557983, -1160.406494, 0.899244),
+    maxDistance = 2.0,
+    objHeading = 99.999977111816,
+    garage = false,
+    fixText = false,
+    audioRemote = false,
+    lockpick = false,
+    slides = false,
+    objHash = -766681263,
+    authorizedGangs = { ['savagemc']=0 },
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- vv2downescapemain 
+Config.DoorList['vv2downescapemain'] = {
+    garage = false,
+    slides = false,
+    fixText = false,
+    maxDistance = 2.0,
+    audioRemote = false,
+    lockpick = false,
+    locked = true,
+    objCoords = vec3(-1153.743164, -1157.008667, 2.903632),
+    objHash = -172886857,
+    objHeading = 17.295951843262,
+    authorizedGangs = { ['savagemc']=0 },
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
+
+-- vv2cell 
+Config.DoorList['vv2cell'] = {
+    garage = false,
+    slides = false,
+    fixText = false,
+    maxDistance = 1.5,
+    audioRemote = false,
+    lockpick = false,
+    locked = true,
+    objCoords = vec3(-1189.803711, -1164.634888, 0.877034),
+    objHash = 631614199,
+    objHeading = 280.0,
+    authorizedGangs = { ['savagemc']=0 },
+    --oldMethod = true,
+    --audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    --audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    --autoLock = 1000,
+    --doorRate = 1.0,
+    --showNUI = true
+}
