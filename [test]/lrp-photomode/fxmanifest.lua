73579eaff53b732ed6500658bab5c7e8aee8530a
haiku <94936310+haiku00@users.noreply.github.com>
2023-11-15 02:24:18 +0200

    new inventory and photomode

---
+++ [test]/lrp-photomode/fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+shared_scripts {
+	'shared/sh_config.lua',
+}
+
+client_scripts {
+    'client/*.lua',
+}
+
+ui_page 'html/ui.html'
+
+files {
+    'html/ui.html',
+	'html/js/*.js',
+	'html/css/*.css',
+	'html/images/*.png',
+	'html/images/*.jpg',
+	'html/audios/*.mp3',
+}
+
+dependencies {
+	'screenshot-basic'
+}
+
+
+
+
