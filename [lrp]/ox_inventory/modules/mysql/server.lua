bbcd29195069cfd22bd273fe5c9ffe9f59bd80e4
Joe Rogan <114414426+arvin-shafiei@users.noreply.github.com>
2023-11-21 00:12:30 +0000

    inv fixes

--- [lrp]/ox_inventory/modules/mysql/server.lua
+++ [lrp]/ox_inventory/modules/mysql/server.lua
@@ -1,9 +1,9 @@
 if not lib then return end
 
 local Query = {
-    SELECT_STASH = 'SELECT 1 AS `exists`, data FROM ox_inventory WHERE owner = ? AND name = ?',
+    SELECT_STASH = 'SELECT data FROM ox_inventory WHERE owner = ? AND name = ?',
     UPDATE_STASH = 'UPDATE ox_inventory SET data = ? WHERE owner = ? AND name = ?',
-    UPSERT_STASH = 'INSERT INTO ox_inventory (owner, name, data) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE data = VALUES(data)',
+    UPSERT_STASH = 'INSERT INTO ox_inventory (data, owner, name) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE data = VALUES(data)',
     INSERT_STASH = 'INSERT INTO ox_inventory (owner, name) VALUES (?, ?)',
     SELECT_GLOVEBOX = 'SELECT plate, glovebox FROM `{vehicle_table}` WHERE `{vehicle_column}` = ?',
     SELECT_TRUNK = 'SELECT plate, trunk FROM `{vehicle_table}` WHERE `{vehicle_column}` = ?',
@@ -129,18 +129,11 @@ function db.savePlayer(owner, inventory)
 end
 
 function db.saveStash(owner, dbId, inventory)
-    return MySQL.prepare(Query.UPSERT_STASH, { owner and tostring(owner) or '', dbId, inventory })
+    return MySQL.prepare(Query.UPSERT_STASH, { inventory, owner and tostring(owner) or '', dbId })
 end
 
 function db.loadStash(owner, name)
-    local parameters = { owner and tostring(owner) or '', name }
-    local response = MySQL.prepare.await(Query.SELECT_STASH, parameters)
-
-    if not response or not response.exists then
-        return MySQL.prepare(Query.INSERT_STASH, parameters)
-    end
-
-    return response.data
+    return MySQL.prepare.await(Query.SELECT_STASH, { owner and tostring(owner) or '', name })
 end
 
 function db.saveGlovebox(id, inventory)
@@ -159,8 +152,9 @@ function db.loadTrunk(id)
     return MySQL.prepare.await(Query.SELECT_TRUNK, { id })
 end
 
+---@param rows number | MySQLQuery | MySQLQuery[]
 local function countRows(rows)
-    if type(rows) ~= 'table' then return rows end
+    if type(rows) == 'number' then return rows end
 
     local n = 0
 
@@ -171,51 +165,82 @@ local function countRows(rows)
     return n
 end
 
+---@param players InventorySaveData[]
+---@param trunks InventorySaveData[]
+---@param gloveboxes InventorySaveData[]
+---@param stashes (InventorySaveData | string | number)[]
+---@param total number[]
 function db.saveInventories(players, trunks, gloveboxes, stashes, total)
-    local numPlayer, numTrunk, numGlove, numStash = #players, #trunks, #gloveboxes, #stashes
     local promises = {}
     local start = os.nanotime()
 
-    shared.info(('Saving %s inventories to the database'):format(total))
+    shared.info(('Saving %s inventories to the database'):format(total[5]))
 
-    if numPlayer > 0 then
+    if total[1] > 0 then
         local p = promise.new()
         promises[#promises + 1] = p
 
         MySQL.prepare(Query.UPDATE_PLAYER, players, function(resp)
-            shared.info(('Saved %s/%s (%.4f ms)'):format(countRows(resp), numPlayer, (os.nanotime() - start) / 1e6))
+            shared.info(('Saved %d/%d players (%.4f ms)'):format(countRows(resp), total[1], (os.nanotime() - start) / 1e6))
             p:resolve()
         end)
     end
 
-    if numTrunk > 0 then
+    if total[2] > 0 then
         local p = promise.new()
         promises[#promises + 1] = p
 
         MySQL.prepare(Query.UPDATE_TRUNK, trunks, function(resp)
-            shared.info(('Saved %s/%s trunks (%.4f ms)'):format(countRows(resp), numTrunk, (os.nanotime() - start) / 1e6))
+            shared.info(('Saved %d/%d trunks (%.4f ms)'):format(countRows(resp), total[2], (os.nanotime() - start) / 1e6))
             p:resolve()
         end)
     end
 
-    if numGlove > 0 then
+    if total[3] > 0 then
         local p = promise.new()
         promises[#promises + 1] = p
 
         MySQL.prepare(Query.UPDATE_GLOVEBOX, gloveboxes, function(resp)
-            shared.info(('Saved %s/%s gloveboxes (%.4f ms)'):format(countRows(resp), numGlove, (os.nanotime() - start) / 1e6))
+            shared.info(('Saved %d/%d gloveboxes (%.4f ms)'):format(countRows(resp), total[3], (os.nanotime() - start) / 1e6))
             p:resolve()
         end)
     end
 
-    if numStash > 0 then
+    if total[4] > 0 then
         local p = promise.new()
         promises[#promises + 1] = p
 
-        MySQL.prepare(Query.UPDATE_STASH, stashes, function(resp)
-            shared.info(('Saved %s/%s stashes (%.4f ms)'):format(countRows(resp), numStash, (os.nanotime() - start) / 1e6))
-            p:resolve()
-        end)
+        if server.bulkstashsave then
+            total[4] /= 3
+
+            MySQL.query(Query.UPSERT_STASH:gsub('%(%?, %?, %?%)', string.rep('(?, ?, ?)', total[4], ', ')), stashes, function(resp)
+                local affectedRows = resp.affectedRows
+
+                if total[4] == 1 then
+                    if affectedRows == 2 then affectedRows = 1 end
+                else
+                    affectedRows -= tonumber(resp.info:match('Duplicates: (%d+)'), 10) or 0
+                end
+
+                shared.info(('Saved %d/%d stashes (%.4f ms)'):format(affectedRows, total[4], (os.nanotime() - start) / 1e6))
+                p:resolve()
+            end)
+        else
+            MySQL.rawExecute(Query.UPSERT_STASH, stashes, function(resp)
+                local affectedRows = 0
+
+                if table.type(resp) == 'hash' then
+                    if resp.affectedRows > 0 then affectedRows = 1 end
+                else
+                    for i = 1, #resp do
+                        if resp[i].affectedRows > 0 then affectedRows += 1 end
+                    end
+                end
+
+                shared.info(('Saved %s/%s stashes (%.4f ms)'):format(affectedRows, total[4], (os.nanotime() - start) / 1e6))
+                p:resolve()
+            end)
+        end
     end
 
     -- All queries must run asynchronously on resource stop, so we'll await multiple promises instead.
